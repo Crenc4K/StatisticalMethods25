60344be5ce0949df4738474a890605ce927146c7
Kyrill <74384466+Crenc4K@users.noreply.github.com>
2025-12-16 13:43:44 +0100

    Delete Binomial_Distribution.R

--- Binomial_Distribution.R
+++
@@ -1,6 +0,0 @@
-calcBinomialDistribution <- function(n, x, p) {
-  return (choose(n, x)*p^x * (1-p)^(n-x))
-}
-
-# example: 10 (n = 10) coin tosses (p = 1/2), exactly 4x heads (x = 4)
-fourheads <- calcBinomialDistribution(10, 4, 0.5)
